4269580a3b2f8cd6d979e722e3638d62d77fd963
abood-74 <abdelrahmanashour45@gmail.com>
2023-03-30 00:00:27 +0200

    stack implementation

--- DS/Stack/stack_array_based.py
+++ DS/Stack/stack_array_based.py
@@ -4,7 +4,7 @@ class Empty(Exception):
 
 
 class Stack:
-    """LIFO Stack implementation using a Python list as underlying storage."""
+    """LIFO Stack implementation using a Python list as underlying storage"""
 
     def __init__(self) -> None:
         """Create an empty stack."""
